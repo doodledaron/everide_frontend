c19f2d2cdf7a516427401cd467fbf937bc6ebeee
Tang Yu Xuan <67047462+doodledaron@users.noreply.github.com>
2024-05-14 01:07:21 +0800

    updated

---
+++ lib/src/widgets/buttons/everide_button.dart
@@ -0,0 +1,49 @@
+import 'package:everide_frontend/src/constants/app_contants.dart';
+import 'package:everide_frontend/src/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class EverideButton extends StatelessWidget {
+  const EverideButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: secondaryColor,
+      borderRadius: BorderRadius.circular(kBorderRadiusButton),
+      onTap: () {
+        //the user wont able to navigate back
+        context.pushNamed("primarymap");
+      },
+      child: Ink(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: SizedBox(
+          height: 60,
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 20,
+              ),
+              const Text(
+                'Everride it now',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Image.asset(
+                'assets/leaf.png',
+                width: 30,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
